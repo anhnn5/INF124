635c81ff734bc7523fbc4c796af97a3b3cfc38d5
anhnn5 <anhnn5@uci.edu>
2020-04-13 23:54:02 -0700

    anhnn5 - add navbar collapsed

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-
+function navbar() {
+    var x = document.getElementById("toggle");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}

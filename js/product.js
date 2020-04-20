3fee6cd826fb39ab1430b523eb9098a827f5b7cd
Jonathan Tran <31745436+vuhoang1608@users.noreply.github.com>
2020-04-20 10:43:33 -0700

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,108 +1,8 @@
-(function () {
-    var itemList = 
-    [
-      {
-        "id": 1,
-        "name": "Blazer",
-        "description": "Straight-cut Blazer",
-        "imgHref": "./blazer.jfif",
-        "price": "33.99",
-        "quantity": 10
-      },
-      {
-        "id": 2,
-        "name": "Blouse",
-        "description": "Wide-cut Blouse",
-        "imgHref": "./blouse.jfif",
-        "price": "14.99",
-        "quantity": 7
-      },
-      {
-        "id": 3,
-        "name": "Croptop",
-        "description": "Tie-back Top",
-        "imgHref": "./croptop.jfif",
-        "price": "10.99",
-        "quantity": 8
-      },
-      {
-        "id": 4,
-        "name": "Dress",
-        "description": "Short Denim Dress",
-        "imgHref": "./dress.jfif",
-        "price": "89.99",
-        "quantity":3
-      },
-      {
-        "id": 5,
-        "name": "Hoodie",
-        "description": "Pullover Hoodie",
-        "imgHref": "./hoodie.jfif",
-        "price": "49.99",
-        "quantity": 5
-      },
-      {
-        "id": 6,
-        "name": "Jacket",
-        "description": "Distressed Denim Jean Jacket",
-        "imgHref": "./jacket.jfif",
-        "price": "99.99",
-        "quantity": 24
-      },
-      {
-        "id": 7,
-        "name": "Jean",
-        "description": "Fashion Jean",
-        "imgHref": "./jean.jfif",
-        "price": "39.99",
-        "quantity": 15
-      },
-      {
-        "id": 8,
-        "name": "Pant",
-        "description": "Paper-bag Pant",
-        "imgHref": "./pant.jfif",
-        "price": "29.99",
-        "quantity": 10
-      },
-      {
-        "id": 9,
-        "name": "Accessories",
-        "description": "Soft Scarf",
-        "imgHref": "./scarf.jfif",
-        "price": "9.99",
-        "quantity": 12
-      },
-      {
-        "id": 10,
-        "name": "Sweater",
-        "description": "Sweater with Dolman Sleeves",
-        "imgHref": "./sweater.jfif",
-        "price": "3.99",
-        "quantity": 10
-      },
-      {
-        "id": 11,
-        "name": "Tshirt",
-        "description": "T-shirt in soft jersey",
-        "imgHref": "./tshirt.jfif",
-        "price": "18.99",
-        "quantity": 12
-      },
-      {
-        "id": 12,
-        "name": "Skirt",
-        "description": "Short Jersey Skirt",
-        "imgHref": "./skirt.jfif",
-        "price": "27.99",
-        "quantity": 20
-      }
-    ];
+(function () {   
 
 function  initItems(){
   var product = localStorage.getItem('product');
-  var productData = JSON.parse(product);  
-  console.log(productData);
+  var productData = JSON.parse(product);    
   
   var left = document.getElementsByClassName('left'); 
 
@@ -135,6 +35,7 @@ function  initItems(){
   var quantityInput = document.createElement('input');
   quantityInput.type = "number";
   quantityInput.id = "number";
+  quantityInput.defaultValue = "1";
   inputDiv1[0].appendChild(quantityInput);
   
   //Create button
@@ -151,8 +52,8 @@ function  initItems(){
   anchor[0].appendChild(link);
 
   //Send object to next page
-  productData.quantity = document.getElementById('number').value;
   link.addEventListener('click', function() { 
+    productData.quantity = document.getElementById('number').value;
     localStorage.setItem('productData', JSON.stringify(productData))
     }, false);
 }
